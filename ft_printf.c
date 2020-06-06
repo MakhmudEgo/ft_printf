f7e128281e752f583cd610c052f6f194c96a981a
makhmudego <makhmudego@yandex.ru>
2020-06-07 01:15:59 +0300

    придумал логику для обработки формата спецификатора

--- ft_printf.c
+++ ft_printf.c
@@ -22,36 +22,61 @@ void			init_format_specifiers(t_format *f_s)
 	f_s->modifier_second = 0x0;
 	f_s->conversion_type = 0x0;
 }
-//' ' && '-'
-//'+' && '-'
-//'0' && ' '
-//'0' && '+'
-//'+' !gnored ' '
-//'-' !gnored '0'
-void		get_format_specifiers(const char *format, t_format *f_s)
+
+/*
+** ' ' && '-'
+** '+' && '-'
+** '0' && ' '
+** '0' && '+'
+** '+' !gnored ' '
+** '-' !gnored '0'
+** %[флаги][ширина][точность][модификаторы][тип преобразования]
+** %[flags][width][accuracy][modifiers][conversion type]
+*/
+
+void			get_format_specifiers(const char *format, t_format *f_s)
 {
 	init_format_specifiers(f_s);
-	while (*format != '\0' && !f_s->conversion_type)
+	while (*format && (*format == ' ' || *format == '-'
+	|| *format == '+' || *format == '0'))
 	{
-		while (*format && (*format == ' ' || *format == '-' ||
-		*format == '+' || *format == '0'))
-		{
-			if (f_s->flag == 1)
-				f_s->flag = *format;
-			else if (f_s->flag_second == 1 && f_s->flag_second != f_s->flag)
-				f_s->flag_second = *format;
-//			if ()
-			format++;
-		}
+		if (f_s->flag == 1 || (*format == '+' && f_s->flag == ' ')
+		|| (*format == '-' && f_s->flag == '0'))
+			f_s->flag = *format;
+		else if ((f_s->flag_second == 1 && f_s->flag != *format) ||
+		((*format == '+' && f_s->flag_second == ' ')
+		|| (*format == '-' && f_s->flag_second == '0')))
+			f_s->flag_second = *format;
 		format++;
-//		else if ();
 	}
+	while (*format && *format >= '0' && *format <= '9')
+	{
+		f_s->width = (f_s->width * 10) + (*format - '0');
+		format++;
+	}
+	if (*format == '.')
+		format++;
+	while (*format && *format >= '0' && *format <= '9')
+	{
+		f_s->accuracy = (f_s->accuracy * 10) + (*format - '0');
+		format++;
+	}
+	while (*format && (*format == 'h' || *format == 'l'
+	|| *format == 'l' || *format == 'L'))
+	{
+		if (!f_s->modifier)
+			f_s->modifier = *format;
+		else if (f_s->modifier == *format && *format != 'L')
+			f_s->modifier_second = *format;
+		format++;
+	}
+	f_s->conversion_type = *format;
 }
 
 static int		ft_printf_assist(const char *format, va_list ap)
 {
-	int i;
-	t_format f_s;
+	int			i;
+	t_format	f_s;
 
 	i = 0;
 	get_format_specifiers(format, &f_s);
@@ -91,16 +116,18 @@ int				ft_printf(const char *format, ...)
 int				main(void)
 {
 	char *s = "hello";
-	int i = 511;
+	char c = 'c';
+	int i = -512;
 	float n = 5;
 //	ft_printf("%s", "world");
-	ft_printf("% -0+d", n);
 /*	ft_printf("%p", s);
 	write(1, "\n", 1);
 	ft_printf("%X", s);*/
 
-	printf("|% -0+d|", 1);
-
+//	ft_printf("%0 0 0 -+193.0000009531d", n);
+//	printf("|% -0+d|", 1);
+	long double dd = 2.2;
+	printf("|%dd|\n", i);
 
 	//	printf("|%.qqthl;'\,z#13d|\n", i);
 	return (0);
@@ -114,7 +141,6 @@ int				main(void)
 
 
 /*
- *
  * else if (*format == 'c' || *format == 's')
 				return 1;
 			else if (*format == 'd' || *format == 'i' || *format == 'u')
