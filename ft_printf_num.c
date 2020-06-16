643ce36198da5d0fa28325a94034b076ea075220
makhmudego <makhmudego@yandex.ru>
2020-06-16 04:40:44 +0300

    обработал чисел со знаком и написал все в 1 ф-ии, теперь нужно поделить на маленькие по 25 строк максимум.

--- ft_printf_num.c
+++ ft_printf_num.c
@@ -12,17 +12,17 @@
 
 #include "libftprintf.h"
 
-static void ft_printf_lu(unsigned long n, int *i)
+static void ft_printf_unsigned(unsigned long n, int *i)
 {
 	char	c;
 
 	if (n > 9)
-		ft_printf_lu(n / 10, i);
+		ft_printf_unsigned(n / 10, i);
 	c = n % 10 + '0';
 	*i += write(1, &c, 1);
 }
 
-static void ft_printf_ls(long long int n, int *i)
+static void ft_printf_signed(long long int n, int *i)
 {
 	char	c;
 	unsigned long long int num;
@@ -30,10 +30,10 @@ static void ft_printf_ls(long long int n, int *i)
 	num = (n < 0) ? -n : n;
 	if (n < 0)
 		*i += write(1, "-", 1);
-	ft_printf_lu(num, i);
+	ft_printf_unsigned(num, i);
 }
 
-static int ft_printf_c(int n, char c)
+static int ft_printf_char(int n, char c)
 {
 	int i;
 
@@ -41,23 +41,6 @@ static int ft_printf_c(int n, char c)
 	while (i < n)
 	{
 		write(1, &c, 1);
-		i++;
-	}
-	return (i);
-}
-
-static int ft_signed_numlen(long long int n)
-{
-	int i;
-	long long num;
-
-	i = (n < 0) ? 1 : 0;
-	if (n == 0)
-		return (1);
-	num = (n < 0) ? -n : n;
-	while (num)
-	{
-		num /= 10;
 		i++;
 	}
 	return (i);
@@ -80,33 +63,66 @@ int	ft_printf_sgnd(va_list ap, t_format *f_s)
 		n = va_arg(ap, long long int);
 	else
 		n = va_arg(ap, int);
-	num_len = ft_signed_numlen(n);//(f_s->flag == '+' || f_s->flag_second == '+') ? 1 + ft_signed_numlen(n) :
+	num_len = ft_signed_numlen(n);
 	if (f_s->flag == '-' || f_s->flag_second == '-')
 	{
 		if (f_s->flag == '+' || f_s->flag_second == '+')
-			i += (n >= 0) ? write(1, "+", 1) : write(1, "-", 1);
+			i += (n >= 0) ? write(1, "+", 1) : 0;
 		else if (f_s->flag == ' ' || f_s->flag_second == ' ')
 			i += write(1, " ", 1);
-		i += ft_printf_c(f_s->accuracy - num_len, '0');
-		ft_printf_ls(n, &i);
+		i += ft_printf_char(f_s->accuracy - num_len, '0');
+		ft_printf_signed(n, &i);
 		f_s->accuracy += (f_s->flag == '+' || f_s->flag_second == '+'
 				|| f_s->flag == ' ' || f_s->flag_second == ' ') ? 1 : 0;
 		num_len += (f_s->flag == '+' || f_s->flag_second == '+'
 				|| f_s->flag == ' ' || f_s->flag_second == ' ') ? 1 : 0;
-		i += ft_printf_c(f_s->width - ((num_len > f_s->accuracy) ? num_len : f_s->accuracy), ' ');
+		i += ft_printf_char(f_s->width - ((num_len > f_s->accuracy) ? num_len : f_s->accuracy), ' ');
 	}
 	else if (f_s->flag == '+' || f_s->flag_second == '+')
 	{
-
-		i += (n >= 0) ? write(1, "+", 1) : write(1, "-", 1);
+		num_len++;
+		f_s->accuracy += (f_s->accuracy > 0) ? 1 : 0;
+		if (f_s->accuracy > 0)
+			i += ft_printf_char(f_s->width - ((num_len > f_s->accuracy) ? num_len : f_s->accuracy), ' ');
+		i += (n >= 0) ? write(1,  "+", 1) : 0;
+		i += ft_printf_char(f_s->accuracy - num_len, '0');
 		if ((f_s->flag == '0' || f_s->flag_second == '0') && !f_s->accuracy)
-			i += ft_printf_c(f_s->width - num_len, '0');
-		if (f_s->accuracy)
-			i += ft_printf_c(f_s->accuracy, '0');
-
-
+			i += ft_printf_char(f_s->width - num_len, '0');
+		ft_printf_signed(n, &i);
 	}
-
+	else if (f_s->flag == ' ' || f_s->flag_second == ' ')
+	{
+		i += write(1, " ", 1);
+		if ((f_s->flag == '0' || f_s->flag_second == '0') && !f_s->accuracy)
+			i += ft_printf_char(f_s->width - (num_len + 1), '0');
+		else if (f_s->accuracy)
+		{
+			i += ft_printf_char(f_s->width - ((f_s->accuracy < num_len) ? num_len + 1 : f_s->accuracy + 1), ' ');
+			i += ft_printf_char(f_s->accuracy - num_len, '0');
+		}
+		else
+			i += ft_printf_char(f_s->width - num_len - 1, ' ');
+		ft_printf_signed(n, &i);
+	}
+	else if (f_s->flag == '0' || f_s->flag_second == '0')
+	{
+		if ((f_s->flag == '0' || f_s->flag_second == '0') && !f_s->accuracy)
+			i += ft_printf_char(f_s->width - num_len, '0');
+		else if (f_s->accuracy)
+		{
+			i += ft_printf_char(f_s->width - ((f_s->accuracy < num_len) ? num_len : f_s->accuracy), ' ');
+			i += ft_printf_char(f_s->accuracy - num_len, '0');
+		}
+		else
+			i += ft_printf_char(f_s->width - num_len - 1, ' ');
+		ft_printf_signed(n, &i);
+	}
+	else
+	{
+		i += ft_printf_char(f_s->width - ((f_s->accuracy < num_len) ? num_len : f_s->accuracy), ' ');
+		i += ft_printf_char(f_s->accuracy - num_len, '0');
+		ft_printf_signed(n, &i);
+	}
 	return (i);
 }
 /*
@@ -124,7 +140,7 @@ int	ft_printf_num(va_list ap, t_format *f_s)
 	i = 0;
 	if (f_s->conversion_type == 'd' || f_s->conversion_type == 'i')
 		return (ft_printf_sgnd(ap, f_s));
-//	ft_printf_lu(va_arg(ap, unsigned long), &i);
+//	ft_printf_unsigned(va_arg(ap, unsigned long), &i);
 
 
 	return (i);
