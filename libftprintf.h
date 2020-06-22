7703c5b81c73d05c9671859250c38b8ec776a932
Makhmud Ego <makhmudego@yandex.ru>
2020-06-22 17:58:20 +0300

    valid and upd proto

--- libftprintf.h
+++ libftprintf.h
@@ -20,32 +20,33 @@
 # include <stdarg.h>
 # include <locale.h>
 
-typedef struct	s_formatspecifiers
+typedef struct	    s_formatspecifiers
 {
-	char		flg;
-	char		flg_ii;
-	int			wdth;
-	int			acc;
-	char		mdf;
-	char		mdf_ii;
-	char		mdf_x;
-	char		cnv_tp;
-}				t_format;
+	char		    flg;
+	char		    flg_ii;
+	int		    	wdth;
+	int		    	acc;
+	char	    	mdf;
+	char	    	mdf_ii;
+	char	    	mdf_x;
+	char	    	cnv_tp;
+}			    	t_format;
 
-int				ft_printf(const char *format, ...);
-int				ft_printf_addr(char c, va_list ap, t_format *f_s);
-void			get_format_specifiers(const char *format,
+int				    ft_printf(const char *format, ...);
+int				    ft_printf_addr(char c, va_list ap, t_format *f_s);
+void			    get_format_specifiers(const char *format,
 				t_format *f_s, va_list ap);
-size_t			ft_strlen(const char *str);
-int				ft_printf_num(va_list ap, t_format *f_s);
-int				ft_printf_unicode(va_list ap, char c_arg);
-int				ft_signed_numlen(long long int n);
-int				ft_unsigned_numlen(unsigned long long int n);
-void			ft_printf_unsigned(unsigned long n, int *i);
-void			ft_printf_signed(long long int n, int *i);
-int				ft_printf_sgnd(va_list ap, t_format *f_s);
-int				ft_printf_char(int n, char c);
-long long int	get_sngd_value(va_list ap, t_format *f_s);
-long long int	get_unsgd_value(va_list ap, t_format *f_s);
+size_t			    ft_strlen(const char *str);
+int				    ft_printf_num(va_list ap, t_format *f_s);
+int				    ft_printf_unicode(va_list ap, char c_arg);
+int				    ft_signed_numlen(long long int n);
+int				    ft_unsigned_numlen(unsigned long long int n);
+void			    ft_printf_unsigned(unsigned long long n, int *i);
+void		    	ft_printf_signed(long long int n, int *i);
+int				    ft_printf_sgnd(va_list ap, t_format *f_s);
+int                 ft_printf_usgnd(va_list ap, t_format *f_s);
+int				    ft_printf_char(int n, char c);
+long long int	    get_sngd_value(va_list ap, t_format *f_s);
+unsigned long long  get_usgnd_value(va_list ap, t_format *f_s);
 
 #endif
