303200a498ec8f0c2359c7d8683aef45cb0f2f8a
Makhmud Ego <makhmudego@yandex.ru>
2020-06-25 17:17:58 +0300

    valid zero and fixed bag with "-"

--- ft_printf_sgnd.c
+++ ft_printf_sgnd.c
@@ -6,13 +6,13 @@
 /*   By: mizola <mizola@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/16 17:11:33 by mizola            #+#    #+#             */
-/*   Updated: 2020/06/16 17:11:34 by mizola           ###   ########.fr       */
+/*   Updated: 2020/06/25 17:13:05 by mizola           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-static void	if_minus(const long long int *n, t_format *f_s,
+static void	if_minus(long long int *n, t_format *f_s,
 		int *num_len, int *i)
 {
 	*i += (*n < 0) ? write(1, "-", 1) : 0;
@@ -22,7 +22,7 @@ static void	if_minus(const long long int *n, t_format *f_s,
 		*i += write(1, " ", 1);
 	*i += ft_printf_char(f_s->acc - (*n < 0 ? (*num_len - 1)
 			: *num_len), '0');
-	ft_printf_signed(((*n < 0) ? -(*n) : *n), i);
+	ft_print_check_zero(n, f_s, i);
 	f_s->acc += (f_s->flg == '+' || f_s->flg_ii == '+'
 			|| f_s->flg == ' ' || f_s->flg_ii == ' ' || (*n < 0)) ? 1 : 0;
 	*num_len += (*n > 0) && (f_s->flg == '+' || f_s->flg_ii == '+'
@@ -55,7 +55,7 @@ static void	if_plus(long long int *n, t_format *f_s,
 		*i += write(1, (*n >= 0) ? "+" : "-", 1);
 		*i += ft_printf_char(f_s->acc - (*num_len - 1), '0');
 	}
-	ft_printf_signed((*n < 0) ? -(*n) : *n, i);
+	ft_print_check_zero(n, f_s, i);
 }
 
 static void	if_space(long long int *n, t_format *f_s,
@@ -83,36 +83,27 @@ static void	if_space(long long int *n, t_format *f_s,
 	*i += f_s->acc == -1 && (f_s->flg != '0' && f_s->flg_ii != '0') ?
 			ft_printf_char(f_s->wdth - *num_len - 1, ' ') : 0;
 	*i += f_s->acc != -1 ? ft_printf_char(f_s->acc - *num_len, '0') : 0;
-	ft_printf_unsigned((*n < 0) ? -(*n) : *n, i);
+	ft_print_check_zero(n, f_s, i);
 }
 
 static void	if_else(long long int *n, t_format *f_s,
 		int *num_len, int *i)
 {
-	*i += (*n < 0) ? write(1, "-", 1) : 0;
-	f_s->acc += f_s->acc && (f_s->flg == '+'
-			|| f_s->flg_ii == '+' || f_s->flg == ' '
-			|| f_s->flg_ii == ' ' || (*n < 0)) ? 1 : 0;
-	*num_len += (f_s->flg == '+' || f_s->flg_ii == '+'
-			|| f_s->flg == ' ' || f_s->flg_ii == ' ') ? 1 : 0;
-	if (f_s->flg == '0' || f_s->flg_ii == '0')
+	f_s->acc += *n < 0 && f_s->acc != -1 ? 1 : 0;
+	if ((f_s->flg == '0' || f_s->flg_ii == '0') && f_s->acc == -1)
 	{
-		if (f_s->acc)
-		{
-			*i += ft_printf_char(f_s->wdth - ((f_s->acc < *num_len)
-					? (*num_len) : f_s->acc), ' ');
-			*i += ft_printf_char(f_s->acc - (*num_len), '0');
-		}
-		else
-			i += ft_printf_char(f_s->wdth - (*num_len), '0');
+		*i += *n < 0 ? write(1, "-", 1) : 0;
+		*i += *num_len > f_s->acc ? ft_printf_char(f_s->wdth - *num_len, '0') :
+				ft_printf_char(f_s->wdth - f_s->acc, '0');
 	}
 	else
 	{
-		*i += ft_printf_char(f_s->wdth - ((f_s->acc < *num_len)
-				? (*num_len) : f_s->acc), ' ');
-		*i += ft_printf_char(f_s->acc - (*num_len), '0');
+		*i += *num_len > f_s->acc ? ft_printf_char(f_s->wdth - *num_len, ' ') :
+				ft_printf_char(f_s->wdth - f_s->acc, ' ');
+		*i += *n < 0 ? write(1, "-", 1) : 0;
 	}
-	ft_printf_signed((*n < 0) ? -(*n) : *n, i);
+	*i += ft_printf_char(f_s->acc - (*num_len), '0');
+	ft_print_check_zero(n, f_s, i);
 }
 
 int			ft_printf_sgnd(va_list ap, t_format *f_s)
@@ -133,7 +124,6 @@ int			ft_printf_sgnd(va_list ap, t_format *f_s)
 	}
 	else if ((f_s->flg == ' ' || f_s->flg_ii == ' '))
 	{
-		num_len += (n < 0) ? -1 : 0;
 		if_space(&n, f_s, &num_len, &i);
 	}
 	else
