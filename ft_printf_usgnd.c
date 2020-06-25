d6b299794178e2e20aea5b7ae6476e3294f5b74d
Makhmud Ego <makhmudego@yandex.ru>
2020-06-25 17:12:03 +0300

    valid zero

--- ft_printf_usgnd.c
+++ ft_printf_usgnd.c
@@ -6,7 +6,7 @@
 /*   By: mizola <mizola@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/22 19:55:24 by mizola            #+#    #+#             */
-/*   Updated: 2020/06/22 20:00:45 by mizola           ###   ########.fr       */
+/*   Updated: 2020/06/25 16:59:23 by mizola           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,8 @@
 static void	if_minus(unsigned long long *n, t_format *f_s,
 		int *num_len, int *i)
 {
-	*i += (*n < 0) ? write(1, "-", 1) : 0;
 	*i += ft_printf_char(f_s->acc - *num_len, '0');
-	ft_printf_unsigned(*n, i);
+	ft_print_check_uzero(n, f_s, i);
 	*i += ft_printf_char(f_s->wdth - ((*num_len > f_s->acc) ?
 			*num_len : f_s->acc), ' ');
 }
@@ -42,49 +41,37 @@ static void	if_plus(unsigned long long *n, t_format *f_s,
 				*num_len : f_s->acc), ' ');
 		*i += ft_printf_char(f_s->acc - (*num_len), '0');
 	}
-	ft_printf_unsigned(*n, i);
+	ft_print_check_uzero(n, f_s, i);
+
 }
 
 static void	if_space(unsigned long long *n, t_format *f_s,
 		int *num_len, int *i)
 {
-	if (*n < 0)
-	{
-		*i += f_s->acc != -1 && f_s->acc < *num_len ?
-				ft_printf_char(f_s->wdth - *num_len - 1, ' ') : 0;
-		*i += f_s->acc != -1 && f_s->acc > *num_len ?
-				ft_printf_char(f_s->wdth - f_s->acc - 1, ' ') : 0;
-		*i += write(1, "-", 1);
-	}
-	else if (f_s->acc < *num_len)
-		*i += write(1, " ", 1);
+	*i += write(1, " ", 1);
 	*i += f_s->acc == -1 && (f_s->flg == '0' || f_s->flg_ii == '0') ?
 			ft_printf_char(f_s->wdth - *num_len - 1, '0') : 0;
-	if (*n >= 0)
-	{
-		*i += f_s->acc != -1 && f_s->acc > *num_len ?
-				ft_printf_char(f_s->wdth - f_s->acc - 1, ' ') : 0;
-		*i += f_s->acc != -1 && f_s->acc < *num_len ?
-				ft_printf_char(f_s->wdth - *num_len - 1, ' ') : 0;
-	}
+	*i += f_s->acc != -1 && f_s->acc > *num_len ?
+			ft_printf_char(f_s->wdth - f_s->acc - 1, ' ') : 0;
+	*i += f_s->acc != -1 && f_s->acc < *num_len ?
+			ft_printf_char(f_s->wdth - *num_len - 1, ' ') : 0;
 	*i += f_s->acc == -1 && (f_s->flg != '0' && f_s->flg_ii != '0') ?
 			ft_printf_char(f_s->wdth - *num_len - 1, ' ') : 0;
 	*i += f_s->acc != -1 ? ft_printf_char(f_s->acc - *num_len, '0') : 0;
-	ft_printf_unsigned(*n, i);
+	ft_print_check_uzero(n, f_s, i);
 }
 
 static void	if_else(unsigned long long *n, t_format *f_s,
 		int *num_len, int *i)
 {
-	*i += (*n < 0) ? write(1, "-", 1) : 0;
 	f_s->acc += f_s->acc && (f_s->flg == '+'
 	|| f_s->flg_ii == '+' || f_s->flg == ' '
-	|| f_s->flg_ii == ' ' || (*n < 0)) ? 1 : 0;
+	|| f_s->flg_ii == ' ') ? 1 : 0; // || (*n < 0)
 	*num_len += (f_s->flg == '+' || f_s->flg_ii == '+'
 		|| f_s->flg == ' ' || f_s->flg_ii == ' ') ? 1 : 0;
 	if (f_s->flg == '0' || f_s->flg_ii == '0')
 	{
-		if (f_s->acc)
+		if (f_s->acc != -1)
 		{
 			*i += ft_printf_char(f_s->wdth - ((f_s->acc < *num_len)
 				? (*num_len) : f_s->acc), ' ');
@@ -99,7 +86,7 @@ static void	if_else(unsigned long long *n, t_format *f_s,
 			? (*num_len) : f_s->acc), ' ');
 		*i += ft_printf_char(f_s->acc - (*num_len), '0');
 	}
-	ft_printf_unsigned(*n, i);
+	ft_print_check_uzero(n, f_s, i);
 }
 
 int			ft_printf_usgnd(va_list ap, t_format *f_s)
@@ -116,10 +103,7 @@ int			ft_printf_usgnd(va_list ap, t_format *f_s)
 	else if (f_s->flg == '+' || f_s->flg_ii == '+')
 		if_plus(&n, f_s, &num_len, &i);
 	else if ((f_s->flg == ' ' || f_s->flg_ii == ' '))
-	{
-		num_len += (n < 0) ? -1 : 0;
 		if_space(&n, f_s, &num_len, &i);
-	}
 	else
 		if_else(&n, f_s, &num_len, &i);
 	return (i);
