a432c55635291f8a9e4151e44d99157f9bbd6281
Makhmud Ego <makhmudego@yandex.ru>
2020-06-26 13:15:50 +0300

    valid x

--- ft_printf_x.c
+++ ft_printf_x.c
@@ -6,45 +6,50 @@
 /*   By: mizola <mizola@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/25 21:56:14 by mizola            #+#    #+#             */
-/*   Updated: 2020/06/25 22:13:41 by mizola           ###   ########.fr       */
+/*   Updated: 2020/06/26 13:10:36 by mizola           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-static void	print_x(char c, unsigned int n, int *i)
+static int	ft_hex(unsigned long long n, t_format *f_s)
 {
-	char	n_hex;
+	int					i;
+	int					hex_len;
+	unsigned long long	tmp_hex;
 
-	if (n >= 16)
-		print_x(c, n >> 4, i);
-	n_hex = (int)(n % 16) + '0';
-	if ((int)n_hex == 58)
-		n_hex = (c == 'X') ? 'A' : 'a';
-	if ((int)n_hex == 59)
-		n_hex = (c == 'X') ? 'B' : 'b';
-	if ((int)n_hex == 60)
-		n_hex = (c == 'X') ? 'C' : 'c';
-	if ((int)n_hex == 61)
-		n_hex = (c == 'X') ? 'D' : 'd';
-	if ((int)n_hex == 62)
-		n_hex = (c == 'X') ? 'E' : 'e';
-	if ((int)n_hex == 63)
-		n_hex = (c == 'X') ? 'F' : 'f';
-	write(1, &n_hex, 1);
-	*i += 1;
+	i = 0;
+	hex_len = n == 0 ? 1 : 0;
+	hex_len += f_s->mdf_x == '#' ? 2 : 0;
+	tmp_hex = f_s->mdf == 'l' ? n : (int)n;
+	while (tmp_hex)
+	{
+		tmp_hex >>= 4;
+		hex_len++;
+	}
+	if (f_s->flg == '-' || f_s->flg_ii == '-')
+		if_minus_x(&n, f_s, &hex_len, &i);
+	else if (f_s->flg == '+' || f_s->flg_ii == '+')
+		if_plus_x(&n, f_s, &hex_len, &i);
+	else if ((f_s->flg == ' ' || f_s->flg_ii == ' '))
+		if_space_x(&n, f_s, &hex_len, &i);
+	else
+		if_else_x(&n, f_s, &hex_len, &i);
+	return (i);
 }
 
-int ft_printf_x(va_list ap, t_format *f_s)
+int			ft_printf_x(va_list ap, t_format *f_s)
 {
 	int i;
 
 	i = 0;
 	if (f_s->mdf == 'h' && f_s->mdf_ii != 'h')
-		print_x(f_s->cnv_tp, (unsigned short)va_arg(ap, int), &i);
+		i += ft_hex((unsigned short)va_arg(ap, int), f_s);
 	else if (f_s->mdf == 'h' && f_s->mdf_ii == 'h')
-		print_x(f_s->cnv_tp, (unsigned char)va_arg(ap, int), &i);
+		i += ft_hex((unsigned char)va_arg(ap, int), f_s);
+	else if (f_s->mdf == 'l')
+		i += ft_hex(va_arg(ap, unsigned long long), f_s);
 	else
-		print_x(f_s->cnv_tp, va_arg(ap, int), &i);
+		i += ft_hex(va_arg(ap, int), f_s);
 	return (i);
 }
