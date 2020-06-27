bcd2df0e9c416cac5aeb7e207c6b2403a5bd3764
Makhmud Ego <makhmudego@yandex.ru>
2020-06-27 17:48:19 +0300

    valid [x]

--- ft_print_check_zero.c
+++ ft_print_check_zero.c
@@ -6,7 +6,7 @@
 /*   By: mizola <mizola@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/25 16:26:02 by mizola            #+#    #+#             */
-/*   Updated: 2020/06/26 12:14:54 by mizola           ###   ########.fr       */
+/*   Updated: 2020/06/27 17:04:19 by mizola           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	ft_print_check_zero(long long int *n, t_format *f_s, int *i)
 		*i += f_s->wdth > 0 ? write(1, " ", 1) : 0;
 	else
 		ft_printf_signed(((*n < 0) ? -(*n) : *n), i);
+	if (*n == 0 && f_s->acc == 0 && f_s->is_pls && !f_s->is_mns)
+		*i += write(1, "+", 1);
 }
 
 void	ft_print_check_uzero(unsigned long long *n, t_format *f_s, int *i)
@@ -30,8 +32,6 @@ void	ft_print_check_uzero(unsigned long long *n, t_format *f_s, int *i)
 
 void	ft_print_check_xzero(unsigned int *n, t_format *f_s, int *i)
 {
-	*i += f_s->flg_x && f_s->cnv_tp == 'x' ? write(1, "0x", 2) : 0;
-	*i += f_s->flg_x && f_s->cnv_tp == 'X' ? write(1, "0X", 2) : 0;
 	if ((f_s->acc == 0 && *n == 0))
 		*i += f_s->wdth > 0 ? write(1, " ", 1) : 0;
 	else
@@ -40,8 +40,6 @@ void	ft_print_check_xzero(unsigned int *n, t_format *f_s, int *i)
 
 void	ft_print_check_lxzero(unsigned long long *n, t_format *f_s, int *i)
 {
-	*i += f_s->flg_x && f_s->cnv_tp == 'x' ? write(1, "0x", 2) : 0;
-	*i += f_s->flg_x && f_s->cnv_tp == 'X' ? write(1, "0X", 2) : 0;
 	if ((f_s->acc == 0 && *n == 0))
 		*i += f_s->wdth > 0 ? write(1, " ", 1) : 0;
 	else
