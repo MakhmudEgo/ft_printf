d736bc290d1d03e5d4cc128858b9281c41f162bc
Makhmud Ego <makhmudego@yandex.ru>
2020-06-27 13:17:27 +0300

    corrected f_s

--- ft_print_check_zero.c
+++ ft_print_check_zero.c
@@ -30,8 +30,8 @@ void	ft_print_check_uzero(unsigned long long *n, t_format *f_s, int *i)
 
 void	ft_print_check_xzero(unsigned int *n, t_format *f_s, int *i)
 {
-	*i += f_s->mdf_x == '#' && f_s->cnv_tp == 'x' ? write(1, "0x", 2) : 0;
-	*i += f_s->mdf_x == '#' && f_s->cnv_tp == 'X' ? write(1, "0X", 2) : 0;
+	*i += f_s->flg_x && f_s->cnv_tp == 'x' ? write(1, "0x", 2) : 0;
+	*i += f_s->flg_x && f_s->cnv_tp == 'X' ? write(1, "0X", 2) : 0;
 	if ((f_s->acc == 0 && *n == 0))
 		*i += f_s->wdth > 0 ? write(1, " ", 1) : 0;
 	else
@@ -40,8 +40,8 @@ void	ft_print_check_xzero(unsigned int *n, t_format *f_s, int *i)
 
 void	ft_print_check_lxzero(unsigned long long *n, t_format *f_s, int *i)
 {
-	*i += f_s->mdf_x == '#' && f_s->cnv_tp == 'x' ? write(1, "0x", 2) : 0;
-	*i += f_s->mdf_x == '#' && f_s->cnv_tp == 'X' ? write(1, "0X", 2) : 0;
+	*i += f_s->flg_x && f_s->cnv_tp == 'x' ? write(1, "0x", 2) : 0;
+	*i += f_s->flg_x && f_s->cnv_tp == 'X' ? write(1, "0X", 2) : 0;
 	if ((f_s->acc == 0 && *n == 0))
 		*i += f_s->wdth > 0 ? write(1, " ", 1) : 0;
 	else
