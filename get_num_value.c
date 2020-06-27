d5b2fa7c910003a2fde00cb160cb5425cd194b4c
Makhmud Ego <makhmudego@yandex.ru>
2020-06-27 16:46:34 +0300

    crd

--- get_num_value.c
+++ get_num_value.c
@@ -6,7 +6,7 @@
 /*   By: mizola <mizola@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/17 00:51:42 by mizola            #+#    #+#             */
-/*   Updated: 2020/06/17 00:53:14 by mizola           ###   ########.fr       */
+/*   Updated: 2020/06/27 16:31:30 by mizola           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ long long int		get_sngd_value(va_list ap, t_format *f_s)
 	else if (f_s->mdf == 'h' && f_s->mdf_ii == 'h')
 		n = (char)va_arg(ap, int);
 	else if (f_s->mdf == 'l' && f_s->mdf_ii != 'l')
-		n = va_arg(ap, long int);
+		n = (long int)va_arg(ap, long int);
 	else if (f_s->mdf == 'l' && f_s->mdf_ii == 'l')
-		n = va_arg(ap, long long int);
+		n = va_arg(ap, long long);
 	else
 		n = va_arg(ap, int);
 	return (n);
@@ -38,9 +38,9 @@ unsigned long long	get_usgnd_value(va_list ap, t_format *f_s)
 	else if (f_s->mdf == 'h' && f_s->mdf_ii == 'h')
 		n = (unsigned char)va_arg(ap, int);
 	else if (f_s->mdf == 'l' && f_s->mdf_ii != 'l')
-		n = va_arg(ap, unsigned long);
+		n = (unsigned long)va_arg(ap, unsigned long);
 	else if (f_s->mdf == 'l' && f_s->mdf_ii == 'l')
-		n = va_arg(ap, unsigned long long int);
+		n = va_arg(ap, unsigned long long);
 	else
 		n = (unsigned int)(va_arg(ap, int));
 	return (n);
